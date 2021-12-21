a86053e962fed5de4b2abbe0d2b8c2d9fcc18ee6
Alexander Derevyanko <70107243+alexstark-spb@users.noreply.github.com>
2021-12-21 17:02:02 +0300

    added LoginTests

---
+++ src/test/java/com/alexstark/tests/LoginTests.java
@@ -0,0 +1,69 @@
+package com.alexstark.tests;
+
+import com.alexstark.config.App;
+import com.alexstark.helpers.AllureRestAssuredFilter;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+
+@Story("Login tests")
+public class LoginTests extends TestBase {
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization (UI)")
+    void loginTest() {
+        step("Open login page", () ->
+                open("/login"));
+
+        step("Fill login form", () -> {
+            $("#Email").setValue(App.config.userLogin());
+            $("#Password").setValue(App.config.userPassword()).pressEnter();
+        });
+
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(App.config.userLogin())));
+    }
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization (API + UI)")
+    void loginWithCookieTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authorizationCookie =
+                    given()
+                            .filter(AllureRestAssuredFilter.withCustomTemplates())
+                            .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                            .formParam("Email", App.config.userLogin())
+                            .formParam("Password", App.config.userPassword())
+                            .when()
+                            .post("http://demowebshop.tricentis.com/login")
+                            .then()
+                            .statusCode(302)
+                            .extract()
+                            .cookie("NOPCOMMERCE.AUTH");
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+
+            step("Set cookie to browser", () ->
+                    getWebDriver().manage().addCookie(
+                            new Cookie("NOPCOMMERCE.AUTH", authorizationCookie)));
+        });
+
+        step("Open main page", () ->
+                open(""));
+
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(App.config.userLogin())));
+    }
+}
